ad6becda0a109850fd19c3170e9de04ddd8cb7d3
jkusnierz01 <jedrek33@onet.pl>
2024-08-18 15:43:57 +0200

    basic page fundamental

--- youtube_summarizer/youtube_summarizer/youtube_summarizer.py
+++ youtube_summarizer/youtube_summarizer/youtube_summarizer.py
@@ -13,20 +13,46 @@ class State(rx.State):
 
 
 def index() -> rx.Component:
-    # Welcome Page (Index)
+    """_summary_
+
+    Returns:
+        rx.Component: _description_
+    """
     return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.center(
-            motion(
-                rx.button(
-                    "Click me"
-                ),
-                while_hover={"scale": 1.2},
-                while_tap={"scale": 0.9},
-                transition={"type": "spring", "stiffness": 400, "damping": 17},
-            )
+
+        rx.center(
+            rx.heading(
+                "YouTube Summarizer",
+                font_size="4em",                  # Large font size for the title
+                font_weight="bold",               # Bold font for emphasis
+                text_align="center",              # Center the text
+                font_family="'Roboto', sans-serif",  # Use a modern, clean font
+                color="transparent",              # Start with transparent text
+                background_clip="text",           # Apply background gradient to text
+                background_image="linear-gradient(90deg, #ff7e5f, #feb47b)",
+                webkit_background_clip="text",    # Ensure compatibility across browsers
+                webkit_text_fill_color="transparent",  # Ensures text color fills with gradient
+                text_shadow="2px 2px 4px rgba(0, 0, 0, 0.5)",
+                margin_top="2em",                 # Add margin to position the text better
+                size='9',
+                as_='h1',
+            ),
+            padding="2em",
         ),
+        rx.center(
+            rx.text(
+                rx.text.strong("Input "), "link to YouTube video under ",
+                font_size="1em",
+                font_family="'Montserrat', sans-serif",
+                margin_bottom = '1em'
+            ),
+            rx.input(
+                radius='full',
+                width = '50%',
+                placeholder="Input url..."
+            ),
+            direction='column',
+            justify='center',
         )
     )
 
